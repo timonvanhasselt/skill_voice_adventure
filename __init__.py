365a81af5e25bac855f3a255e0d422885510679f
timonvanhasselt <timonvanhasselt@users.noreply.github.com>
2023-09-22 15:42:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,49 +6,56 @@ from ovos_workshop.skills import OVOSSkill
 from pydub import AudioSegment
 from pydub.playback import play
 import threading
+import random
+
 
 
 class VoiceAdventureSkill(OVOSSkill):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.score = 0
-        self.question_keys = list(QUESTIONS_AUDIO_PATHS.keys())
-        self.total_questions = len(self.question_keys)
-        self.current_question_index = 0
+        self.current_option_index = 0
 
         # Start the background audio loop in a separate thread
         self.background_thread = threading.Thread(target=self.play_background_audio_loop)
         self.background_thread.daemon = True
         self.background_thread.start()
 
+    
+# Define the audio file paths for question 1
+QUESTION_1_AUDIO_PATHS = {
+    "question": "q1.mp3",
+    "options": ["q1a1.mp3", "q1a2.mp3", "q1a3.mp3", "q1a4.mp3"],
+    "correct_answer": "q1correct.mp3",
+    "incorrect_answer": "q1incorrect.mp3",
+    "intro": "q1intro.mp3",
+    "outro": "q1outro.mp3"
+}
+
+# Define the audio file paths for question 2
+QUESTION_2_AUDIO_PATHS = {
+    "question": "q2.mp3",
+    "options": ["q2a1.mp3", "q2a2.mp3", "q2a3.mp3", "q2a4.mp3"],
+    "correct_answer": "q2correct.mp3",
+    "incorrect_answer": "q2incorrect.mp3",
+    "intro": "q2intro.mp3",
+    "outro": "q2outro.mp3"
+}
+
+# Define the audio file paths for question 3
+# ... (repeat this structure for each question up to 10)
+
+# ...
+
    def initialize(self):
         # Initialize the OCP audio service with the bus
         self.audio = OCPInterface(self.bus)
 
-    # Replace with your actual audio file path
-BACKGROUND_AUDIO_LOOP = "path/to/background_audio_loop.mp3"
 
-# Replace with your actual audio file paths
-QUESTIONS_AUDIO_PATHS = {
-    "Question 1": {
-        "question": "path/to/question1.mp3",
-        "correct_answer": "path/to/question1_yes.mp3",
-        "incorrect_answer": "path/to/question1_no.mp3"
-    },
-    "Question 2": {
-        "question": "path/to/question2.mp3",
-        "correct_answer": "path/to/question2_yes.mp3",
-        "incorrect_answer": "path/to/question2_no.mp3"
-    },
-    # Add paths for other questions in a similar manner
-}
     def play_background_audio_loop(self):
-       # background_audio = AudioSegment.from_mp3(BACKGROUND_AUDIO_LOOP)
-        background_audio = BACKGROUND_AUDIO_LOOP
-        
+        background_audio = AudioSegment.from_mp3(BACKGROUND_AUDIO_LOOP)
         while True:
-            #play(background_audio)
-            self.audio.play(background_audio)
+            play(background_audio)
 
     @classproperty
     def runtime_requirements(self):
@@ -66,42 +73,57 @@ QUESTIONS_AUDIO_PATHS = {
 
     @intent_handler(IntentBuilder("StartGameIntent").require("StartGameKeyword"))
     def handle_start_game_intent(self, message):
-        self.speak_dialog("welcome")
         self.ask_question()
 
     def ask_question(self):
-        if self.current_question_index < self.total_questions:
-            question_key = self.question_keys[self.current_question_index]
-            question_info = QUESTIONS_AUDIO_PATHS[question_key]
+        question_number = self.current_question_index + 1
+        question_key = f"Question {question_number}"
+        question_info = globals()[f"QUESTION_{question_number}_AUDIO_PATHS"]
 
-            self.speak(question_info["question"])
-            self.speak(question_info["correct_answer"])
-            self.speak(question_info["incorrect_answer"])
+        self.speak(question_info["intro"])
+        self.speak(question_info["question"])
 
-            self.current_question_index += 1
-        else:
-            self.speak_dialog("end_of_game")
-            self.speak(f"Your final score is {self.score}")
-            self.stop()
+        # Randomize the order of options for the current question
+        options = question_info["options"]
+        random.shuffle(options)
 
-    @intent_handler(IntentBuilder("YesIntent").require("YesKeyword"))
-    def handle_yes_intent(self, message):
-        self.handle_answer(True)
+        # Play the randomized options for the current question
+        for option in options:
+            play(option)
 
-    @intent_handler(IntentBuilder("NoIntent").require("NoKeyword"))
-    def handle_no_intent(self, message):
-        self.handle_answer(False)
+        # Reset the option index for the new question
+        self.current_option_index = 0
+
+    @intent_handler(IntentBuilder("AnswerIntent").require("AnswerKeyword"))
+    def handle_answer_intent(self, message):
+        user_response = message.data.get("AnswerKeyword")
+        if user_response == "yes":
+            self.handle_answer(True)
+        elif user_response == "no":
+            self.handle_answer(False)
 
     def handle_answer(self, is_correct):
+        question_number = self.current_question_index + 1
+        question_key = f"Question {question_number}"
+        question_info = globals()[f"QUESTION_{question_number}_AUDIO_PATHS"]
+
         if is_correct:
-            self.speak_dialog("correct_answer")
+            self.speak(question_info["correct_answer"])
             self.score += 1
         else:
-            self.speak_dialog("incorrect_answer")
+            self.speak(question_info["incorrect_answer"])
 
-        self.ask_question()
+        self.current_question_index += 1
+        if self.current_question_index < 10:
+            self.ask_question()
+        else:
+            self.speak_dialog("end_of_game", {"score": self.score})
+            self.stop()
 
     def stop(self):
         # Stop the background audio loop thread
         self.background_thread.join()
         pass
+
+
+
